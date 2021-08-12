7a061b34412c737eacba38790f1206905bff2804
yazanabdulhafez <yazanali31@yahoo.com>
2021-08-12 23:01:37 +0300

    finsh code chalenge 10

--- javascript/code-challenges/challenges-08.test.js
+++ javascript/code-challenges/challenges-08.test.js
@@ -10,47 +10,47 @@ Write a function named sortByChildren that sorts the characters below by the num
 
 ------------------------------------------------------------------------------------------------ */
 let characters = [{
-    name: 'Eddard',
-    spouse: 'Catelyn',
-    children: ['Robb', 'Sansa', 'Arya', 'Bran', 'Rickon'],
-    house: 'Stark'
-  },
-  {
-    name: 'Jon A.',
-    spouse: 'Lysa',
-    children: ['Robin'],
-    house: 'Arryn'
-  },
-  {
-    name: 'Cersei',
-    spouse: 'Robert',
-    children: ['Joffrey', 'Myrcella', 'Tommen'],
-    house: 'Lannister'
-  },
-  {
-    name: 'Daenarys',
-    spouse: 'Khal Drogo',
-    children: ['Drogon', 'Rhaegal', 'Viserion'],
-    house: 'Targaryen'
-  },
-  {
-    name: 'Mace',
-    spouse: 'Alerie',
-    children: ['Margaery', 'Loras'],
-    house: 'Tyrell'
-  },
-  {
-    name: 'Euron',
-    spouse: null,
-    children: [],
-    house: 'Greyjoy'
-  },
-  {
-    name: 'Jon S.',
-    spouse: null,
-    children: [],
-    house: 'Snow'
-  }
+  name: 'Eddard',
+  spouse: 'Catelyn',
+  children: ['Robb', 'Sansa', 'Arya', 'Bran', 'Rickon'],
+  house: 'Stark'
+},
+{
+  name: 'Jon A.',
+  spouse: 'Lysa',
+  children: ['Robin'],
+  house: 'Arryn'
+},
+{
+  name: 'Cersei',
+  spouse: 'Robert',
+  children: ['Joffrey', 'Myrcella', 'Tommen'],
+  house: 'Lannister'
+},
+{
+  name: 'Daenarys',
+  spouse: 'Khal Drogo',
+  children: ['Drogon', 'Rhaegal', 'Viserion'],
+  house: 'Targaryen'
+},
+{
+  name: 'Mace',
+  spouse: 'Alerie',
+  children: ['Margaery', 'Loras'],
+  house: 'Tyrell'
+},
+{
+  name: 'Euron',
+  spouse: null,
+  children: [],
+  house: 'Greyjoy'
+},
+{
+  name: 'Jon S.',
+  spouse: null,
+  children: [],
+  house: 'Snow'
+}
 ];
 
 const sortByChildren = (charArray) => {
